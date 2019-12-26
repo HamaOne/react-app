16e0a66e0faf73be1c01c37281110d780965f2e0
Heok Rae Lim <heokree.lim@gmail.com>
2019-12-26 09:04:36 +0900

    Updates

---
+++ precache-manifest.393db03898114b232a656ae836d3c468.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c906d279b91927d08716d37164a5c340",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "6cfa7505b8fefdc65b9b",
+    "url": "/react-app/static/css/main.d6279a93.chunk.css"
+  },
+  {
+    "revision": "cc599fe0d054eade7bf4",
+    "url": "/react-app/static/js/2.85792254.chunk.js"
+  },
+  {
+    "revision": "6cfa7505b8fefdc65b9b",
+    "url": "/react-app/static/js/main.942c9ec3.chunk.js"
+  },
+  {
+    "revision": "9b417d75f9f42c249c23",
+    "url": "/react-app/static/js/runtime-main.5f0f8821.js"
+  }
+]);
